bec1494f5d3a0059a653da6cce0e91ff1f583864
Muhammad Amin <muhammadamin137340@gmail.com>
2026-03-08 13:14:01 +0500

    Initialize Express server and set up basic route

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
  
+const express = require('express');
+const app = express();
+let pot=3000
+
+
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the School Management System' });
+});
+
+app.listen(pot, () => console.log(`Server running on port ${pot}`));
